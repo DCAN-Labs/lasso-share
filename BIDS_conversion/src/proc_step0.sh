b44146f27f8bc3dae3903303b51cbb71fb667e58
LuciMoore <lmoore@umn.edu>
2026-09-16 12:52:22 -0700

    add initial code

---
+++ BIDS_conversion/src/proc_step0.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -l
+
+#SBATCH -J PIP_proc0
+#SBATCH -c 24
+#SBATCH --mem=50G
+#SBATCH -t 8:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=fayzu001@umn.edu
+#SBATCH -p msismall
+#SBATCH -o output_logs/PIP_proc0_%A_%a.out
+#SBATCH -e output_logs/PIP_proc0_%A_%a.err
+#SBATCH -A moser297
+
+singularity=`which singularity`
+source /projects/standard/faird/shared/code/external/envs/miniconda3/load_miniconda3.sh
+conda activate py11
+
+SUB=${1}
+SES=${2}
+
+#set up directories
+PARENT=/projects/standard/moser297/shared/projects/PIP
+#dcm_s3_dir=s3://subpop/dicoms/${SUB}-ses${SES}/
+dcm_dir=${PARENT}/data/DICOMs/${SUB}/${SUB}_ses${SES}/
+rawbids_dir=${PARENT}/bids
+fmriprep_in_dir=${PARENT}/derivatives/nordic
+
+#run init
+echo python /projects/standard/faird/shared/code/internal/utilities/cdniproc_v2.0/init.py -p ${SUB} -s ${SES} -d ${dcm_dir} --jsononly
+python /projects/standard/faird/shared/code/internal/utilities/cdniproc_v2.0/init.py -p ${SUB} -s ${SES} -d ${dcm_dir} --jsononly
+
+
+#alternate path
+#dcm2bids_helper -d data/DICOMs/MN0002/MN0002_sesV1PFM1 -o helper/MN0002_sesV1PFM1
+#python /projects/standard/faird/shared/code/internal/utilities/cdniproc_v2.0/nii_to_tsv_gpt4.py -p ${SUB} -s ${SES} ${nii_dir} #nii_dir: helper/MN0002_sesV1PFM1/tmp_dcm2bids/helper/
+#python /projects/standard/faird/shared/code/internal/utilities/cdniproc_v2.0/tsv_to_json.py {subj.tsv}
+
+# Check if there's an "imagetype" field in the json, if so remove
+jq 'del(.. | .ImageType?)' ${SUB}_ses${SES}.json > tmp.json && mv tmp.json ${SUB}_ses${SES}.json
+
+#mv to summaries dir
+echo 'mv' ${SUB}_ses${SES}* summaries/.
+mv ${SUB}_ses${SES}* summaries/.
+
+#sync full dicom directory
+#echo s3cmd sync --recursive --no-check-md5 --skip-existing ${dcm_s3_dir} ${dcm_scratch_dir}
+#s3cmd sync --recursive --no-check-md5 --skip-existing ${dcm_s3_dir} ${dcm_scratch_dir}
+
+#run_dcm2bids3
+echo python /projects/standard/faird/shared/code/internal/utilities/cdniproc/dcm2bids3_dev.py -f summaries/${SUB}_ses${SES}.tsv -d ${dcm_dir} --all 
+python /projects/standard/faird/shared/code/internal/utilities/cdniproc/dcm2bids3_dev.py -f summaries/${SUB}_ses${SES}.tsv -d ${dcm_dir} --all
+
+#mv bids folder
+echo 'make bids ses' folder
+if [ ! -d ${PARENT}/bids/sub-${SUB} ]; then
+  mkdir -p ${PARENT}/bids/sub-${SUB}
+fi 
+mv bids/sub-${SUB}/ses-${SES} ${PARENT}/bids/sub-${SUB}/.
---
+++ BIDS_conversion/src/proc_step1.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -l
+
+#SBATCH -J PIP_proc1
+#SBATCH -c 24
+#SBATCH --mem=50G
+#SBATCH -t 8:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=fayzu001@umn.edu
+#SBATCH -p msismall
+#SBATCH -o output_logs/PIP_proc1_%A_%a.out
+#SBATCH -e output_logs/PIP_proc1_%A_%a.err
+#SBATCH -A moser297
+
+singularity=`which singularity`
+source /projects/standard/faird/shared/code/external/envs/miniconda3/load_miniconda3.sh
+conda activate py11
+
+SUB=${1}
+SES=${2}
+
+#set up directories
+PARENT=/projects/standard/moser297/shared/projects/PIP
+rawbids_dir=${PARENT}/bids
+fmriprep_in_dir=${PARENT}/derivatives/nordic
+
+#make nordic cmds
+echo python /projects/standard/faird/shared/code/internal/utilities/cdniproc/tools/make_nordic_cmds_dev.py ${rawbids_dir}/sub-${SUB}/ses-${SES}/func/
+python /projects/standard/faird/shared/code/internal/utilities/cdniproc/tools/make_nordic_cmds_dev.py ${rawbids_dir}/sub-${SUB}/ses-${SES}/func/
+
+#mv to summaries dir
+echo 'mv' ${PARENT}/nordic_cmd_sub-${SUB}_ses-${SES}.sh ${PARENT}/summaries/.
+mv ${PARENT}/nordic_cmd_sub-${SUB}_ses-${SES}.sh ${PARENT}/summaries/.
+
+echo 'make derivatives/nordic' folder
+if [ ! -d ${fmriprep_in_dir} ]; then
+  mkdir -p ${fmriprep_in_dir}
+fi 
+
+declare -a JOBIDS=()
+echo 'submit NORDIC jobs'
+nordicfile=${PARENT}/summaries/nordic_cmd_sub-${SUB}_ses-${SES}.sh
+while IFS= read -r line; do
+	#echo $line
+	$line
+done < "$nordicfile"
+
+
+if [ ${#JOBIDS[@]} -eq 0 ]; then
+        echo "All Permutations already run. No jobs were submitted. Exiting and moving on to figure making."
+        # If you need to exit script entirely, uncomment the next line
+        # exit 0
+    else
+        # Function to check the completion status of a job using sacct
+        check_job_completion() {
+            local job=$1
+            jobState=$(sacct -j $job --format=State --noheader | head -n 1 | awk '{print $1}')
+        
+            if [[ $jobState =~ ^(COMPLETED|FAILED|CANCELLED)$ ]]; then
+                return 1 # Job is completed (no longer running)
+            else
+                return 0 # Job is still running
+            fi        }
+
+        # Wait for all jobs to complete
+        ALL_DONE=0
+        while [ $ALL_DONE -eq 0 ]; do
+            ALL_DONE=1
+            for JOBID in "${JOBIDS[@]}"; do
+                check_job_completion $JOBID
+                if [ $? -eq 0 ]; then
+                    echo "Waiting for job $JOBID to complete..."
+                    ALL_DONE=0
+                    sleep 60
+                    break # Exit the for loop and wait before checking again
+                fi
+            done
+        done
+
+        echo "All SPLITPCT perm jobs completed"
+
+    fi
+
+#combine sessions
+#run post nordic
+#set IntendedFors
